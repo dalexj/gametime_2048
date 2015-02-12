02276493b48f667e8481088526bb901f943247ac
Alex <96dalex@gmail.com>
2015-02-12 10:45:12 -0700

    everything is working finally, fixed some tests

--- 2048.js
+++ 2048.js
@@ -26,11 +26,11 @@ function restartGame() {
 }
 
 function pushLeft() {
-  push(true, false);
+  push(true, true);
 }
 
 function pushRight() {
-  push(true, true);
+  push(true, false);
 }
 function pushDown() {
   push(false, false);
@@ -42,16 +42,16 @@ function pushUp() {
 function push(horizontal, increasing) {
   var changed = true;
   var tiles = initializeTiles();
-
   while(changed) {
     changed = false;
-
-    var currentIndex = 0;
-    for (var row = 0; row < 4; row++) {
-      for (var col = 0; col < 4; col++) {
-        var nextIndex = calculateNextIndex(currentIndex, horizontal, increasing);
-        changed = swapOrCombineTiles(tiles, currentIndex, nextIndex) || changed;
-        currentIndex += 1;
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 3; j++) {
+        var currentIndex = calculateCurrentIndex(i, j, horizontal, increasing);
+        var nextIndex    = calculateNextIndex(currentIndex, horizontal, increasing);
+        changed = swapTiles(tiles, currentIndex, nextIndex) || changed;
+        if(!changed) {
+          changed = combineTiles(tiles, currentIndex, nextIndex) || changed;
+        }
       }
     }
   }
@@ -59,84 +59,38 @@ function push(horizontal, increasing) {
   drawSquares();
 }
 
+function calculateCurrentIndex(i, j, horizontal, increasing) {
+  var row = horizontal ? i : j;
+  var col = horizontal ? j : i;
+  if(!increasing) {
+    row = 3 - row;
+    col = 3 - col;
+  }
+  return row*4 + col;
+}
+
 function calculateNextIndex(currentIndex, horizontal, increasing) {
-  var nextIndex = 0;
-  var herbertIsDumb = false;
+  var toAdd = horizontal ? 1 : 4;
+  if(!increasing) toAdd *= -1;
 
-  var left  = horizontal  && !increasing;
-  var right = horizontal  && increasing;
-  var up    = !horizontal && increasing;
-  var down  = !horizontal && !increasing;
-
-  left    ? nextIndex = calculateLeft(currentIndex)   :
-  right   ? nextIndex = calculateRight(currentIndex)  :
-  up      ? nextIndex = calculateUp(currentIndex)     :
-  down    ? nextIndex = calculateDown(currentIndex)   :
-  herbertIsDumb = true;
-
-  if (herbertIsDumb) return "Damn, Herbert's dumb..."; else  return nextIndex;
+  return currentIndex + toAdd;
 }
 
-function calculateLeft(currentIndex) {
-  var result = 0;
-
-  currentIndex ===  0 ? result = currentIndex :
-  currentIndex ===  4 ? result = currentIndex :
-  currentIndex ===  8 ? result = currentIndex :
-  currentIndex === 12 ? result = currentIndex :
-  result = currentIndex - 1;
-
-  return result;
-}
-
-function calculateRight(currentIndex) {
-  var result = 0;
-
-  currentIndex ===  3 ? result = currentIndex :
-  currentIndex ===  7 ? result = currentIndex :
-  currentIndex === 11 ? result = currentIndex :
-  currentIndex === 15 ? result = currentIndex :
-  result = currentIndex + 1;
-
-  return result;
-}
-
-function calculateUp(currentIndex) {
-  var result = 0;
-
-  currentIndex === 0 ? result = currentIndex :
-  currentIndex === 1 ? result = currentIndex :
-  currentIndex === 2 ? result = currentIndex :
-  currentIndex === 3 ? result = currentIndex :
-  result = currentIndex - 4;
-
-  return result;
-}
-
-function calculateDown(currentIndex) {
-  var result = 0;
-
-  currentIndex === 12 ? result = currentIndex :
-  currentIndex === 13 ? result = currentIndex :
-  currentIndex === 14 ? result = currentIndex :
-  currentIndex === 15 ? result = currentIndex :
-  result = currentIndex + 4;
-
-  return result;
-}
-
-function swapOrCombineTiles(tiles, currIndex, nextIndex) {
-  var result = 0;
-
+function swapTiles(tiles, currIndex, nextIndex) {
   var currTile = tiles[currIndex];
   var nextTile = tiles[nextIndex];
-  if (currTile.score !== 0 && nextTile.score === 0) {
+  if (currTile.score === 0 && nextTile.score !== 0) {
     tiles[currIndex] = nextTile;
     tiles[nextIndex] = currTile;
     return true;
-  } else if (nextTile.score !== 0) {
-    tiles[currIndex] = currTile;
-  } else if (currTile.score !== 0 && currTile.score === nextTile.score && !currTile.combined && !nextTile.combined) {
+  }
+  return false;
+}
+
+function combineTiles(tiles, currIndex, nextIndex) {
+  var currTile = tiles[currIndex];
+  var nextTile = tiles[nextIndex];
+  if (currTile.score !== 0 && currTile.score === nextTile.score && !currTile.combined && !nextTile.combined) {
     currTile.score   *= 2;
     nextTile.score    = 0;
     currTile.combined = true;
