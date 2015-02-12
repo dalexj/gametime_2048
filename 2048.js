21e2aff69ac2caf7535aa5a5e80915d165ff2b44
Alex <96dalex@gmail.com>
2015-02-11 18:33:00 -0700

    some refactoring, room for herbert to finish the rest

--- 2048.js
+++ 2048.js
@@ -31,13 +31,13 @@ function pushLeft() {
 }
 
 function pushRight() {
-  push(true, false)
+  push(true, false);
 }
 function pushDown() {
-  push(false, false)
+  push(false, false);
 }
 function pushUp() {
-  push(false, true)
+  push(false, true);
 }
 
 function push(horizontal, increasing) {
@@ -45,24 +45,14 @@ function push(horizontal, increasing) {
   var tiles = initializeTiles();
   while(changed) {
     changed = false;
-    for (var row = 0; row < 4; row++) {
-      for (var col = 0; col < 3; col++) {
-        var currentIndex = horizontal ? row*4 + col : col*4 + row;
-        var direction    = horizontal ? 1 : 4;
-        var destinationIndex = increasing ? (currentIndex + direction) : setDirection(currentIndex, direction);
-
-        var currentTile = tiles[currentIndex];
-        var nextTile    = tiles[destinationIndex];
-        if (currentTile.score === 0 && nextTile.score != 0) {
-          tiles[currentIndex]     = nextTile;
-          tiles[destinationIndex] = currentTile;
-          changed = true;
-        } else if(currentTile.score != 0 && currentTile.score === nextTile.score && !currentTile.combined && !nextTile.combined) {
-          currentTile.score   *= 2;
-          nextTile.score       = 0;
-          currentTile.combined = true
-          changed = true;
-        }
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 3; j++) {
+        // var currentIndex = horizontal ? row*4 + col : col*4 + row;
+        // var direction    = horizontal ? 1 : 4;
+        // var nextIndex = increasing ? (currentIndex + direction) : setDirection(currentIndex, direction);
+        var currentIndex = calculateCurrentIndex(i, j, horizontal);
+        var nextIndex    = calculateNextIndex(currentIndex, horizontal, increasing);
+        changed = swapOrCombineTiles(tiles, currentIndex, nextIndex) || changed;
       }
     }
   }
@@ -70,11 +60,36 @@ function push(horizontal, increasing) {
   drawSquares();
 }
 
+function calculateNextIndex(currentIndex, horizontal, increasing) {
+  return "herbert do this";
+}
+
+function calculateCurrentIndex(i, j, horizontal) {
+  var row = horizontal ? i : j;
+  var col = horizontal ? j : i;
+  return "herbert do this";
+}
+
+function swapOrCombineTiles(tiles, currIndex, nextIndex) {
+  var currTile = tiles[currIndex];
+  var nextTile = tiles[nextIndex];
+  if (currTile.score === 0 && nextTile.score !== 0) {
+    tiles[currIndex] = nextTile;
+    tiles[nextIndex] = currTile;
+    return true;
+  } else if(currTile.score !== 0 && currTile.score === nextTile.score && !currTile.combined && !nextTile.combined) {
+    currTile.score   *= 2;
+    nextTile.score    = 0;
+    currTile.combined = true;
+    return true;
+  }
+  return false;
+}
 
 function placeRandomSquare() {
   var indexesThatHaveSquares = [];
   for (var i = 0; i < scores.length; i++) {
-    if(scores[i] != 0) indexesThatHaveSquares.push(i);
+    if(scores[i] !== 0) indexesThatHaveSquares.push(i);
   }
   if(indexesThatHaveSquares.length === 16) return;
   scores[randomNumberNotIn(indexesThatHaveSquares)] = generateTwoOrFour();
@@ -82,20 +97,20 @@ function placeRandomSquare() {
 
 function setDirection(currentIndex, direction) {
   if (direction === 4 && currentIndex < 4 || currentIndex > 11) {
-    return currentIndex
+    return currentIndex;
   } else if (direction === 1 && currentIndex === 0)  {
-    return currentIndex
+    return currentIndex;
   } else {
-    return currentIndex - direction
+    return currentIndex - direction;
   }
 }
 
 function initializeTiles() {
-  var tile_objects = []
+  var tileObjects = [];
   for (var i = 0; i < scores.length; i++) {
-    tile_objects.push( { score: scores[i], combined: false } );
+    tileObjects.push( { score: scores[i], combined: false } );
   }
-  return tile_objects
+  return tileObjects;
 }
 
 $(document).ready(function() {
